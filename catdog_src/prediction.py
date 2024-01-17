1130e69a953116905f057b94a8e9aebdfd6862f3
Arun02DS <arun02negime@gmail.com>
2024-01-17 16:15:43 +0530

    prediction code

--- catdog_src/prediction.py
+++ catdog_src/prediction.py
@@ -1,6 +1,11 @@
 from catdog_src.logger import logging
 from catdog_src.exception import CatDogException
 import os,sys
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+
 
 
 
@@ -12,7 +17,7 @@ class CatDog:
         except Exception as e:
             raise CatDogException(e,sys)
 
-    def predictioncatdog(self)->int:
+    def predictioncatdog(self)->str:
         
         """
         Description: This function loads model and predict outcome.
@@ -22,7 +27,30 @@ class CatDog:
         """
 
         try:
-            
+            #loading model
+            logging.info(f"loading model")
+            model = load_model(os.path.join('model','model_clf.h5'))
+
+            logging.info(f"loading test image")
+            imagename=self.filename
+
+            test_image=image.load_img(imagename,target_size=(64,64))
+            test_image=image.img_to_array(test_image)
+            test_image=np.expand_dims(test_image,axis=0)
+            result=np.argmax(model.predict(test_image),axis=1)
+
+            logging.info(f"result: {result}")
+            print(result)
+
+            if result[0]==1:
+                prediction='dog'
+                logging.info(f"Image provided is {prediction}")
+                return [{"image":prediction}]
+            else:
+                prediction='cat'
+                logging.info(f"Image provided is {prediction}")
+                return [{"image":prediction}]
+
         except Exception as e:
             raise CatDogException(e,sys)
 
